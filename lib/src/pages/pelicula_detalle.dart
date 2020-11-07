dc26fdb5c4f7692618fd5a0054860fd7f2bcf35f
Freddy <freddy.artea@gmail.com>
2020-11-06 22:18:56 -0500

    Diseno de la pantalla detalle

--- lib/src/pages/pelicula_detalle.dart
+++ lib/src/pages/pelicula_detalle.dart
@@ -4,12 +4,98 @@ import 'package:peliculas/src/models/pelicula_model.dart';
 class PeliculaDetalle extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    //asigamos los argumentos a pelicula
     final Pelicula pelicula = ModalRoute.of(context).settings.arguments;
-
     return Scaffold(
-        body: Center(
-      child: Text(pelicula.title),
+        body: CustomScrollView(
+      slivers: <Widget>[
+        _crearAppbar(pelicula),
+        SliverList(
+          delegate: SliverChildListDelegate([
+            SizedBox(height: 10.0),
+            _posterTitulo(context, pelicula),
+            _descripcion(pelicula),
+            _descripcion(pelicula),
+            _descripcion(pelicula),
+            _descripcion(pelicula),
+          ]),
+        )
+      ],
     ));
+  }
+
+  Widget _crearAppbar(Pelicula pelicula) {
+    return SliverAppBar(
+      elevation: 2.0,
+      backgroundColor: Colors.indigoAccent,
+      expandedHeight: 250.0,
+      // floating: false,
+      pinned: true,
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        title: Text(
+          pelicula.title,
+          style: TextStyle(color: Colors.white, fontSize: 16.0),
+        ),
+        background: Image(
+          image: NetworkImage(pelicula.getBackgroundImg()),
+          // placeholder: AssetImage('assets/img/loading.gif'),
+          // fadeInDuration: Duration(microseconds: 1000),
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+
+  Widget _posterTitulo(BuildContext context, Pelicula pelicula) {
+    return Container(
+        padding: EdgeInsets.symmetric(horizontal: 20.0),
+        child: Row(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(20.0),
+              child: Image(
+                image: NetworkImage(pelicula.getPosterImg()),
+                height: 150.0,
+              ),
+            ),
+            SizedBox(width: 20.0),
+            // widget que se adapta a todo el espacio restante
+            Flexible(
+                child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  pelicula.title,
+                  style: Theme.of(context).textTheme.headline6,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                Text(
+                  pelicula.originalTitle,
+                  style: Theme.of(context).textTheme.subtitle1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                Row(
+                  children: [
+                    Icon(Icons.star_border),
+                    Text(
+                      pelicula.voteAverage.toString(),
+                      style: Theme.of(context).textTheme.subtitle1,
+                    )
+                  ],
+                )
+              ],
+            ))
+          ],
+        ));
+  }
+
+  Widget _descripcion(Pelicula pelicula) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+      child: Text(
+        pelicula.overview,
+        textAlign: TextAlign.justify,
+      ),
+    );
   }
 }
